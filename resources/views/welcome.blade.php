ffeb936a3eb88e48fdba446b2a5af0ce760e6ab4
Vadim <Vadim_lachkov@mail.ru>
2021-06-06 13:04:50 +0700

    laravel+sass+bootstrap

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="css/app.css">
+
+    </head>
+    <body class="antialiased">
+
+    <div class="container">
+        <div class="col-lg-6 offset-lg-3 text-center fw-bold">
+            <h1 class="text-primary">LP project</h1>
+        </div>
+    </div>
+
+    </body>
+</html>
